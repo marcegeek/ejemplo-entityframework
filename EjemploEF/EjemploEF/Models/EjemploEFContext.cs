16c8a336299ffab007c3d95c6fe06d0c143406d4
Marcelo Castellano <marce.geek22@gmail.com>
2017-02-28 21:25:30 -0300

    Agrego contexto de datos

---
+++ EjemploEF/EjemploEF/Models/EjemploEFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EjemploEF.Models
+{
+    public class EjemploEFContext : DbContext
+    {
+        public EjemploEFContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
