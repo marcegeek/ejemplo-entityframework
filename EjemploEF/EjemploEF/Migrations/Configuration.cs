33162779129216b3a0cf1881454de97b630a0700
Marcelo Castellano <marce.geek22@gmail.com>
2017-03-01 01:24:54 -0300

    Habilito migraciones automáticas

---
+++ EjemploEF/EjemploEF/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace EjemploEF.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EjemploEF.Models.EjemploEFContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "EjemploEF.Models.EjemploEFContext";
+        }
+
+        protected override void Seed(EjemploEF.Models.EjemploEFContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
