40f8434410e3dd66e10f202877f9083c94947024
Marcelo Castellano <marce.geek22@gmail.com>
2017-02-28 21:28:22 -0300

    Agrego modelo de ciudades

---
+++ EjemploEF/EjemploEF/Models/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EjemploEF.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [StringLength(50, ErrorMessage = "El campo tiene que tener entre 3 y 50 caracteres",
+            MinimumLength = 3)]
+        [Index("CityName_Index", IsUnique = true)]
+        public string Name { get; set; }
+    }
+}
